2fa2caf6ea12c7dabfb007aa11c553cd32802fa4
xiaoqianqian <xiaoqianqian@papayamobile.cn>
2019-04-28 17:37:49 +0800

    引入echarts

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="echarts" :style="{ width: '1200px', height: '600px'}"></div>
   </div>
 </template>
+<script>
+
+
+export default {
+    mounted () {
+        this.draw();
+    },
+    created () {
+        
+    },
+    methods:{
+        draw(){
+            let echarts = this.$echarts.init(document.getElementById('echarts')),
+                    _this = this;
+
+            echarts.setOption({
+                title: {
+                    text: 'ECharts 入门示例'
+                },
+                tooltip: {},
+                xAxis: {
+                    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            })
+        }
+    }
+}
+</script>
+
